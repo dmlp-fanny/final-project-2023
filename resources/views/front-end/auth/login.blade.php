6804d141342705acc6a7f64d88de327bf49e23b4
tuyennguyen <tuyenntc@gmail.com>
2023-06-28 15:26:22 +0200

    style

---
+++ resources/views/front-end/auth/login.blade.php
@@ -0,0 +1,38 @@
+{{-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style-login.css">
+    <title>Login Form</title>
+</head>
+<body>
+    <div class="form-login">
+        <h1>Login</h1>
+        <form action="">
+            <div class="inputbox">
+                <label for="email"></label>
+                <input type="text" id="email" name="email" placeholder="Email" required>
+                
+                <br>
+                <label for="password"></label>
+                <input type="password" id="password" name="password" placeholder="Password" required>
+               
+                <br>
+            </div>
+                <div class="remember-me">
+                <label><input type="checkbox">Remember me</label>
+            
+                <a href="#">Forgot password?</a>
+            </div>
+            <br>
+            <button type="submit" class="btn">Login</button>
+
+            <div class="register">
+                <p>Don't have an account? <a href="#">Sign up</a></p>
+            </div>
+
+        </form>
+    </div>
+</body>
+</html> --}}
