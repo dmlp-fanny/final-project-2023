48165e6b2b4682c7bff1d484c959bafe821d1081
dmlp-fanny <118313057+dmlp-fanny@users.noreply.github.com>
2023-06-29 18:23:52 +0200

    seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\Language;
 use App\Models\Request;
 use App\Models\Tag;
 use App\Models\Timeslot;
@@ -30,19 +31,18 @@ class DatabaseSeeder extends Seeder
         $tags = Tag::factory(10)->create();
 
         Request::factory(10)
-            ->state(new Sequence(fn (Sequence $sequence) => ['user_id' => User::all()->random()]))
+            ->state(new Sequence(fn () => ['user_id' => User::all()->random()]))
             ->create()
             ->each(function ($request) use ($tags) {
-                $request->tags()->sync($tags->random(mt_rand(1,2)));
+                $request->tags()->sync($tags->random(mt_rand(1, 2)));
             });
 
         Translator::factory(10)
             ->create()
             ->each(function ($translator) use ($tags) {
-                $translator->tags()->sync($tags->random(mt_rand(1,4)));
-            })
-            ->each(function ($translator) {
+                $translator->tags()->sync($tags->random(mt_rand(1, 4)));
                 $translator->timeslots()->sync(Timeslot::all()->random(mt_rand(5, 10)));
+                $translator->fromLanguages()->syncWithPivotValues(Language::all()->random(), ['to_language_id' => Language::all()->random()->id]);
             });
     }
 }
