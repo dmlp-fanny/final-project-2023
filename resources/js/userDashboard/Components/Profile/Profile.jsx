563f1b4d9a8d761623cbd00b44a59db9a7641dad
dmlp-fanny <118313057+dmlp-fanny@users.noreply.github.com>
2023-06-30 14:42:00 +0200

    profile page

--- resources/js/userDashboard/Components/Profile/Profile.jsx
+++ resources/js/userDashboard/Components/Profile/Profile.jsx
@@ -21,18 +21,28 @@ export default function Profile() {
 
     return (
         <main>
-            <div className="profile_container">
-                <h2>Your Profile</h2>
-                <h3>Personal Information:</h3>
-                <div className="profile_container_section">
+            {user ? (
+                <div className="profile_container">
+                    <h2>Your Profile</h2>
+                    <h3>Personal Information:</h3>
+                    <div className="profile_container_section-bold">
+                        First Name:<span>{user.first_name}</span> <br />
+                        Last Name:<span>{user.last_name}</span> <br />
+                        Username: <span>{user.username}</span> <br />
+                        Email: <span>{user.email}</span> <br />
+                        Location: <span>{user.location + ', ' + user.country}</span> <br />
+                        Phone Number: <span>{user.phone_number}</span> <br />
+                    </div>
+                    <h3>Experience:</h3>
+                    <div className="profile_container_section">
+                        {user.translator.experience}
+                    </div>
+                    <h3>Categories:</h3>
+                    <div className="profile_container_section"></div>
                 </div>
-                <h3>Experience:</h3>
-                <div className="profile_container_section">
-                    {user.translator.experience}
-                </div>
-                <h3>Categories:</h3>
-                <div className="profile_container_section"></div>
-            </div>
+            ) : (
+                ""
+            )}
         </main>
     );
 }
