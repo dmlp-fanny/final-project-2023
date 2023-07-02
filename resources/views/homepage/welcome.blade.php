fb396e5a7dbb3ab0fedb4d3ed3156be24b3ba721
tuyennguyen <tuyenntc@gmail.com>
2023-07-02 12:35:46 +0200

    add navbar

--- resources/views/homepage/welcome.blade.php
+++ resources/views/homepage/welcome.blade.php
@@ -1,1 +1,25 @@
-<h1>hi</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../../app.css">
+    <title>Landing page</title>
+</head>
+<body>
+    <header>
+        <img src="" alt="">
+        <nav>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#services">Services</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+        <a href="#login" class="login-button">Login</a>
+        <a href="#signup" class="signup-button">Sign Up</a>
+    </header>
+</body>
+</html>
